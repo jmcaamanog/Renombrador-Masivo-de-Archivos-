3d48da91b908f48a71dcfdaee1413a8cfbdb3bd8
Jose Manuel Caamaño <cybor2001@gmail.com>
2026-07-01 17:40:18 +0200

    Add ASCII art header to Renombrador-Masivo-de-Archivos.py
    Added ASCII art header to the script.

--- CODE/Renombrador-Masivo-de-Archivos.py
+++ CODE/Renombrador-Masivo-de-Archivos.py
@@ -1,3 +1,11 @@
+"""
+      ██╗███╗   ███╗ ██████╗
+      ██║████╗ ████║██╔════╝
+      ██║██╔████╔██║██║
+ ██   ██║██║╚██╔╝██║██║    
+ ╚█████╔╝██║ ╚═╝ ██║╚██████╗
+  ╚════╝ ╚═╝     ╚═╝ ╚═════╝
+"""
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 import os
